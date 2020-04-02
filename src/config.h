bd05dce76cd71a20886144874231ca0ce72422fb
jx95 <jx95@duke.edu>
2020-04-02 00:23:02 -0400

    change directory

--- src/config.h
+++ src/config.h
@@ -12,7 +12,7 @@
 // =====================================================
 
 #define BUFFER_SIZE 20
-#define MYLOG "throughput.log"
+#define MYLOG "/var/log/erss/throughput.log"
 #include <iostream>
 #include <cstring>
 #include <sys/socket.h>
